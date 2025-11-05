1ff48bb1405c11b8e74d4cb5ccef577c68ca1b3c
david.cubides <david.cubides@sinco.com.co>
2025-11-05 15:53:23 -0500

    🔴Si_NumeroIngresadoEsUno_Debe_ConvertirAUno

---
+++ NumerosRomanos.Test/NumerosRomanosTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+namespace NumerosRomanos.Test;
+
+public class NumerosRomanosTest
+{
+    [Fact]
+    public void Si_NumeroIngresadoEsUno_Debe_ConvertirAUno()
+    {
+        //Arrange
+        var numeroRomano = new NumeroRomano();
+
+        //Act
+        string resultado = numeroRomano.Convertir(1);
+
+        //Assert
+        resultado.Should().Be("1");
+    }
+}
+
+public class NumeroRomano
+{
+    public string Convertir(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
